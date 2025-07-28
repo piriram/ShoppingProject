eb1e733c5531823025ae326468ebb702810bfa22
piriram <pyoram25@gmail.com>
2025-07-28 16:58:33 +0900

    Refacotor: 함수 오버로딩 적용해봄

--- ShoppingProject/NaverShoppingAPI.swift
+++ ShoppingProject/NaverShoppingAPI.swift
@@ -5,6 +5,7 @@
 //  Created by piri kim on 7/26/25.
 //
 // MARK: 네트워크 레이어
+// 싱글톤 패턴의 이점?
 import Foundation
 import Alamofire
 
@@ -21,8 +22,19 @@ final class NaverShoppingAPI {
     
     private init() {}
     
-    func fetchProducts(query:String,display:Int,sort:String,completion: @escaping (Result<ProductTotal, Error>) -> Void) {
-        // TODO: Sort 추가하기
+    // MARK: 함수 오버로딩으로 사용해봄
+    // 1. sort 기본값 제공
+    func fetchProducts(query: String, display: Int, completion: @escaping (Result<ProductTotal, Error>) -> Void) {
+        fetchProducts(query: query, display: display, sort: "sim", completion: completion)
+    }
+
+    // 2. display 기본값도 제공
+    func fetchProducts(query: String, completion: @escaping (Result<ProductTotal, Error>) -> Void) {
+        fetchProducts(query: query, display: 30, sort: "sim", completion: completion)
+    }
+
+    // 3. 실제 로직 처리하는 함수 (기존 함수)
+    func fetchProducts(query: String, display: Int, sort: String, completion: @escaping (Result<ProductTotal, Error>) -> Void) {
         let parameters: [String: String] = [
             "query": query,
             "display": "\(display)",
@@ -43,7 +55,7 @@ final class NaverShoppingAPI {
                 completion(.failure(error))
             }
         }
-        
     }
+
 }
 
